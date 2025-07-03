05d0ad1db6b7fc0376c7f6b7279001e6df20387c
Code-ever <166537703+code-ever@users.noreply.github.com>
2025-07-03 18:47:12 +0100

    Fix background image and improve landing page UI

--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -8,16 +8,16 @@ const LandingPage = () => {
             <div
                 className='bg-[#d9b348] w-full h-screen'
             >
-                <div className=' p-10 '>
-                    <img src={arizo} alt="logo" className=' rounded-lg' style={{ width: '150px' }} />
+                <div className=' p-6 '>
+                    <img src={arizo} alt="logo" className=' rounded-lg w-24'  />
                 </div>
 
                 <div className='flex justify-between p-3'>
                     <div className=' p-8 w-full md:w-1/2 text-white'>
-                        <p className='py-4'>Welcome Arizona chipping</p>
+                        <p className='py-4'>Welcome to Arizona chipping</p>
                         <p className='text-xl md:text-2xl'>Please select location you are ordering from</p>
                         <form className='py-8 md:py-4'>
-                            <select className='p-3 rounded-md focus:bg-[#d9b348] text-sm text-gray-400 w-full'>
+                            <select className='p-4 rounded-md focus:bg-[#d9b348] text-sm text-gray-400 w-full'>
                                 <option value="">Select Location</option>
                                 <option value="Enugu">Enugu</option>
                                 <option value="Anambra">Anambra</option>
