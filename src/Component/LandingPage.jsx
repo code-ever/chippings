11a776815a2fac4e8a1a7e2c2c3aa06ff6892af6
Code-ever <166537703+code-ever@users.noreply.github.com>
2025-07-03 19:24:59 +0100

    Fix background image and improve landing page UI

--- src/Component/LandingPage.jsx
+++ src/Component/LandingPage.jsx
@@ -9,14 +9,14 @@ const LandingPage = () => {
                
                 className='bg-[#2a2a2c] w-full h-screen fixed top-0 left-0 bottom-0'
             >
-                <div className='pl-10 p-6 '>
+                <div className='pl-10 pt-6 '>
                     <img src={arizo} alt="logo" className=' rounded-lg w-24' />
                 </div>
 
                 <div className='flex justify-between p-3'>
-                    <div className=' p-8 w-full md:w-1/2 text-[#d9b348]'>
+                    <div className=' pt-4 pl-8 w-full md:w-1/2 text-[#d9b348]'>
                         <p className='py-4'>Welcome to Arizona chipping</p>
-                        <p className='text-2xl md:text-2xl'>Please select location you are ordering from</p>
+                        <p className='text-2xl md:text-2xl'>Please select location you are ordering from.</p>
                         <form className='py-8 md:py-4'>
                             <select className='p-4 rounded-md focus:bg-[#d9b348] text-sm text-gray-400 w-full'>
                                 <option value="">Select Location</option>
@@ -26,7 +26,7 @@ const LandingPage = () => {
                             </select>
                             <p style={{ marginTop: '10px' }}>Select from the dropdown below</p>
                             <button type="submit" className='mt-[10px] rounded-lg text-[#2a2a2c] bg-white p-3 w-40 py-4'>
-                                Search
+                                Start ordering
                             </button>
                         </form>
                     </div>
